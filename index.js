9289ee4cd839e6cab4e8e1fbc57a49bc594427b9
Esin <nbusra.esin@gmail.com>
2023-04-17 15:18:35 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+// Your code goes here,,
+
+let text = document.querySelector("#text")
+
+console.log(text)
+
+document.addEventListener("DOMContentLoaded", function(){
+    text.innerHTML = "This is really cool!"
+})
